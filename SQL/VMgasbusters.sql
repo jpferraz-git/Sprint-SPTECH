894b5c023864bf714f22152c0e3a84af6066a1c5
Miguel-an-gel <miguel.mamani@sptech.school>
2024-10-10 08:09:51 -0300

    Atualizando o script do banco

--- SQL/VMgasbusters.sql
+++ SQL/VMgasbusters.sql
@@ -1,70 +1,68 @@
 CREATE DATABASE gasbusters;
 USE gasbusters;
 
-CREATE TABLE empresas (
-idEmpresa 		INT AUTO_INCREMENT PRIMARY KEY,
-nomeEmpresa 	VARCHAR(100),
-cnpj			VARCHAR(18),
-cidade			Varchar(50),
-bairro			VARCHAR(50),
-uf				CHAR(2),
-rua				VARCHAR(100),
-numero 			VARCHAR(6),
-cep				VARCHAR(9),
-telefoneEmpresa VARCHAR(20),
-emailEmpresa 	VARCHAR(100),
+CREATE TABLE empresa(
+idEmpresa  		INT PRIMARY KEY AUTO_INCREMENT,
+razaoSocial  	VARCHAR(100) NOT NULL,
+nomeFantasia 	VARCHAR(100),
+CNPJ  			CHAR(14) NOT NULL,
+cidade  		VARCHAR(45) NOT NULL,
+bairro 			VARCHAR(45) NOT NULL,
+Uf 				CHAR(2) NOT NULL,
+rua 			VARCHAR(45) NOT NULL,
+numero 			VARCHAR(6) NOT NULL,
+cep 			CHAR(9) NOT NULL,
+telefoneEmpresa CHAR(10),
+emailEmpresa 	VARCHAR(100) NOT NULL,
 dtCriacao 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE usuarios (
-idUsuario 		INT AUTO_INCREMENT PRIMARY KEY,
-nome 			VARCHAR(100),
-telefonePessoa 	VARCHAR(20),
-cpf				VARCHAR(14),
-dtNascimento 	DATE,
-emailPessoal 	VARCHAR(100),
-senha 			VARCHAR(30),
-dtCriacao 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-fkEmpresa 		INT,
-FOREIGN KEY (fkEmpresa) REFERENCES empresas(idEmpresa)
+CREATE TABLE usuario(
+idUsuario		INT NOT NULL,
+nome  			VARCHAR(45) NOT NULL,
+telefonePessoa  CHAR(11) NOT NULL,
+cpf  			CHAR(11) NOT NULL,
+dtNasc  		DATE NOT NULL,
+emailPessoal  	VARCHAR(100) NOT NULL,
+senha  			VARCHAR(45) NOT NULL,
+dtCriacao  		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkEmpresa  		INT NOT NULL,
+fkUsuarioADM  	INT NOT NULL,
+PRIMARY KEY (idUsuario, fkUsuarioADM),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+FOREIGN KEY (fkUsuarioADM)REFERENCES usuario (idUsuario)
 );
 
-
-CREATE TABLE cozinhas (
-idCozinha 			INT AUTO_INCREMENT PRIMARY KEY,
-tipoCozinha 		VARCHAR(15),
-numeroFuncionarios	INT,
-observacoes			TEXT,
-entradaGas			INT, 
-fkEmpresa			INT,
-dtCriacao 			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY (fkEmpresa) REFERENCES empresas(idEmpresa),
+CREATE TABLE cozinha(
+idCozinha		 	 INT NOT NULL,
+tipoCozinha 		 VARCHAR(15) NOT NULL,
+numeroFuncionarios 	 INT NOT NULL,
+observacoes 		 TEXT,
+entradasGas 		 INT NOT NULL,
+fkEmpresa 			 INT NOT NULL,
+PRIMARY KEY (idCozinha,fkEmpresa),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
 CHECK (tipoCozinha IN ('Industrial', 'Comercial'))
 );
 
-
-
-CREATE TABLE sensores (
-idSensor 		INT AUTO_INCREMENT PRIMARY KEY,
-nomeSensor		VARCHAR(50),
-localInstalacao VARCHAR(255),
-sensorStatus 	VARCHAR(20),
-dtInstalacao	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-fkEmpresa		INT,
-fkCozinha		INT,
-FOREIGN KEY (fkEmpresa) REFERENCES empresas(idEmpresa),
-FOREIGN KEY (fkCozinha) REFERENCES cozinhas(idCozinha),
-CHECK (sensorStatus IN ('Ativo', 'Inativo')) 
+CREATE TABLE sensor(
+idSensor            INT NOT NULL,
+nomeSensor          VARCHAR(45) NOT NULL,
+localInstalacao     VARCHAR(255) NOT NULL,
+sensorStatus        VARCHAR(20) NOT NULL,
+dtInstalacao        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkCozinha           INT NOT NULL,
+PRIMARY KEY (idSensor, fkCozinha),
+FOREIGN KEY (fkCozinha) REFERENCES cozinha (idCozinha),
+CHECK (sensorStatus IN ('Ativo', 'Inativo'))
 );
 
-
-CREATE TABLE medida (
-idMedida		 INT AUTO_INCREMENT PRIMARY KEY,
-sensor_analogico FLOAT,
-dtLeitura		 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-fkSensor		 INT,
-FOREIGN KEY (fkSensor) REFERENCES sensores(idSensor) 
+CREATE TABLE  medida(
+idMedida  			INT NOT NULL,
+sensor_analogico 	FLOAT NOT NULL,
+dtLeitura  			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkSensor  			INT NOT NULL,
+PRIMARY KEY (idMedida, fkSensor),
+FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
 );
-
 
