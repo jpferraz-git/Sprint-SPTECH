9f24b4f648d25dabd8dacf71738e4f278c413513
lucasaiello-dev <lucas.aiello@sptech.school>
2024-10-16 18:44:48 -0300

    Finalizando a validação de senhas da Tela de Cadastro

--- WebSite/Site/Cadastro_e_Login/public/script.js
+++ WebSite/Site/Cadastro_e_Login/public/script.js
@@ -32,6 +32,8 @@ $(document).ready(function () {
 // Validações
 
 function validarCampos() {
+    var validacao_senha = false
+    var validacao_confirmacao = false
 
     // Validando Senha
     const senha = input_senha_cadastro.value
@@ -47,14 +49,14 @@ function validarCampos() {
         letra_atual = senha[letra]
         if (isNaN(parseInt(letra_atual)) == false) {
             qtd_num++
-        } else{
+        } else {
             for (var caract = 0; caract < caract_especiais.length; caract++) {
                 if (letra_atual == caract_especiais[caract]) {
                     qtd_caract_especiais++
                     letra_caract_especial = true
-                } 
+                }
             }
-            if(letra_caract_especial == false) {
+            if (letra_caract_especial == false) {
                 if (letra_atual == letra_atual.toUpperCase() && letra_atual != ' ') {
                     qtd_letras_maius++
                 } else if (letra_atual == ' ') {
@@ -69,18 +71,26 @@ function validarCampos() {
     if (senha.length < 8 || qtd_num < 2 || qtd_caract_especiais < 2 || qtd_letras_maius < 2 || possui_espaco) {
         alert('A senha deve conter no mínimo 8 caracteres, dentre eles: no mínimo 2 números, 2 caracteres especiais, 2 letras maiúsculas e NÃO deve ter espaços!')
         trocarCorErro()
+    } else {
+        validacao_senha = true
     }
 
     // Validando se a Senha e a Confirmação são iguais
     const confirmacao_senha = input_confirmacao_senha.value
 
-    if (senha != confirmacao_senha){
+    if (senha != confirmacao_senha) {
         alert('O campo Senha e o campo Confirmação estão diferentes!')
         trocarCorErro()
+    } else {
+        validacao_confirmacao = true
+    }
+
+    if (validacao_senha && validacao_confirmacao) {
+        ja_preenchi_dados.href = '../../Dashboard/index.html'
     }
 }
 
-function trocarCorErro(){
+function trocarCorErro() {
     input_senha_cadastro.style = 'color: red'
     input_confirmacao_senha.style = 'color: red'
 
@@ -95,7 +105,7 @@ function trocarCorErro(){
     }
 }
 
-function trocarCorNormal(){
+function trocarCorNormal() {
     input_senha_cadastro.style = 'default'
     input_confirmacao_senha.style = 'default'
 }
