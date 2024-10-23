dcee6225c08d1438f032fb9ef35ba242e081cb22
Th-Sanchez <thiago.peluci@sptech.school>
2024-10-23 11:53:42 -0300

    Remoção dos comentarios do código do arduino

--- API's/dat-acqu-ino/main.js
+++ API's/dat-acqu-ino/main.js
@@ -10,106 +10,68 @@ const SERVIDOR_PORTA = 3300;
 // habilita ou desabilita a inserção de dados no banco de dados
 const HABILITAR_OPERACAO_INSERIR = true;
 
-// habilita ou desabilita a simulação de dados sem Arduino
-const SIMULAR_ARDUINO = true;
-
 // função para comunicação serial
 const serial = async (
     valoresSensorAnalogico
 ) => {
 
     let poolBancoDados = mysql.createPool(
-            {
-                host: '127.0.0.1',
-                user: 'aluno',
-                password: 'Sptech#2024',
-                database: 'GasBusters',
-                port: 3307
-            }
-        ).promise();
-
-    // conexão com o banco de dados MySQL
-    // let poolBancoDados = mysql.createPool(
-    //     {
-    //         host: '127.0.0.1',
-    //         user: 'aluno',
-    //         password: 'Sptech#2024',
-    //         database: 'gasbusters',
-    //         port: 3307
-    //     }
-    // ).promise();
-
-    // PARA HABILITAR A 'IF' ABAIXO, COLOQUE (TRUE) NO 'SIMULAR_ARDUINO'
-    if (SIMULAR_ARDUINO) {
-        // Simula dados do sensor
-        setInterval(() => {
-            const sensorAnalogico = 0;  // valor entre 0 e 100
-            valoresSensorAnalogico.push(parseFloat(sensorAnalogico));
-            console.log(`Simulação de valores, Sensor Analógico: ${sensorAnalogico}`);
-
-            // Insere no banco se habilitado, para habilitar coloque (TRUE) na 'HABILITAR_OPERACAO_INSERIR'
-            if (HABILITAR_OPERACAO_INSERIR) {
-                poolBancoDados.execute(
-                    'INSERT INTO medida (sensor_analogico) VALUES (?)',
-                    [sensorAnalogico]
-                ).then(() => {
-                    console.log("Valores simulados inseridos no banco");
-                }).catch((erro) => {
-                    console.error("Erro ao inserir valores simulados no banco: ", erro);
-                });
-            }
-        }, 2000);  // Simula dados a cada 1 segundo
-    } else {
-
-        // lista as portas seriais disponíveis e procura pelo Arduino
-        const portas = await serialport.SerialPort.list();
-        const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
-        if (!portaArduino) {
-            throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+        {
+            host: '127.0.0.1',
+            user: 'aluno',
+            password: 'Sptech#2024',
+            database: 'GasBusters',
+            port: 3307
         }
+    ).promise();
 
-        // configura a porta serial com o baud rate especificado
-        const arduino = new serialport.SerialPort(
-            {
-                path: portaArduino.path,
-                baudRate: SERIAL_BAUD_RATE
-            }
-        );
-
-        // evento quando a porta serial é aberta
-        arduino.on('open', () => {
-            console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
-        });
-
-        // processa os dados recebidos do Arduino
-        arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
-            console.log(data);
-            const valores = data;
-            const sensorAnalogico = parseFloat(valores);
-            
-
-            // armazena os valores dos sensores nos arrays correspondentes
-            valoresSensorAnalogico.push(sensorAnalogico);
-
-            // insere os dados no banco de dados (se habilitado)
-            if (HABILITAR_OPERACAO_INSERIR) {
-
-                // este insert irá inserir os dados na tabela "medida"
-                await poolBancoDados.execute(
-                    'INSERT INTO medida (sensor_analogico) VALUES (?)',
-                    [sensorAnalogico]
-                );
-                console.log("valores inseridos no banco: ", sensorAnalogico);
-
-            }
-
-        });
-
-        // evento para lidar com erros na comunicação serial
-        arduino.on('error', (mensagem) => {
-            console.error(`Erro no arduino (Mensagem: ${mensagem}`)
-        });
+    // lista as portas seriais disponíveis e procura pelo Arduino
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
     }
+
+    // configura a porta serial com o baud rate especificado
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+
+    // evento quando a porta serial é aberta
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+
+    // processa os dados recebidos do Arduino
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        console.log(data);
+        const valores = data;
+        const sensorAnalogico = parseFloat(valores);
+
+
+        // armazena os valores dos sensores nos arrays correspondentes
+        valoresSensorAnalogico.push(sensorAnalogico);
+
+        // insere os dados no banco de dados (se habilitado)
+        if (HABILITAR_OPERACAO_INSERIR) {
+
+            // este insert irá inserir os dados na tabela "medida"
+            await poolBancoDados.execute(
+                'INSERT INTO medida (sensor_analogico) VALUES (?)',
+                [sensorAnalogico]
+            );
+            console.log("valores inseridos no banco: ", sensorAnalogico);
+
+        }
+    });
+
+    // evento para lidar com erros na comunicação serial
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
 }
 
 // função para criar e configurar o servidor web
