8da2c04d145252a60b67102433ce4f1fc5318ab9
lucasaiello-dev <lucas.aiello@sptech.school>
2024-12-04 11:44:05 -0300

    Adicionando um diretório com mais testes para integrar com o outro já existente

---
+++ API's/testesDataViz/src/database/script-tabelas.sql
@@ -0,0 +1,164 @@
+CREATE DATABASE gasbusters;
+USE gasbusters;
+
+
+CREATE TABLE empresa(
+idEmpresa  		INT PRIMARY KEY AUTO_INCREMENT,
+razaoSocial  	VARCHAR(100),
+nomeFantasia 	VARCHAR(100),
+responsavelLegal VARCHAR(45),
+CNPJ  			CHAR(18),
+cidade  		VARCHAR(45),
+bairro 			VARCHAR(45),
+uf 				CHAR(5),
+rua 			VARCHAR(45),
+numero 			VARCHAR(6),
+cep 			CHAR(9),
+telefoneEmpresa CHAR(14),
+emailEmpresa 	VARCHAR(100),
+dtCriacao 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE usuario(
+idUsuario		INT AUTO_INCREMENT,
+nome  			VARCHAR(45),
+dtNasc  		DATE,
+email			VARCHAR(100),
+senha  			VARCHAR(45),
+dtCriacao  		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkEmpresa  		INT,
+PRIMARY KEY (idUsuario),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+
+CREATE TABLE cozinha(
+idCozinha		 	 INT AUTO_INCREMENT,
+tipoCozinha 		 VARCHAR(15),
+numeroFuncionarios 	 INT,
+observacoes 		 TEXT,
+qtdSaidaGas	 		 INT,
+fkEmpresa 			 INT,
+PRIMARY KEY (idCozinha,fkEmpresa),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+CHECK (tipoCozinha IN ('Industrial', 'Comercial'))
+);
+
+CREATE TABLE localSensor(
+idLocal INT AUTO_INCREMENT,
+descricao TEXT,
+fkCozinha INT,
+fkEmpresa INT,
+PRIMARY KEY (idLocal, fkCozinha, fkEmpresa),
+FOREIGN KEY (fkCozinha) REFERENCES cozinha(idCozinha),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE sensor(
+idSensor            INT PRIMARY KEY AUTO_INCREMENT,
+nomeSensor          VARCHAR(45),
+localInstalacao     VARCHAR(255),
+sensorStatus        VARCHAR(20),
+dtInstalacao        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkCozinha           INT,
+fkEmpresa 			INT,
+fkLocal				INT,
+FOREIGN KEY (fkCozinha) REFERENCES cozinha (idCozinha),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+FOREIGN KEY (fkLocal) REFERENCES localSensor (idLocal),
+CHECK (sensorStatus IN ('Ativo', 'Inativo'))
+);
+
+CREATE TABLE  medida(
+idMedida  			INT AUTO_INCREMENT,
+nivel_gas		 	FLOAT,
+dtLeitura  			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkSensor  			INT,
+PRIMARY KEY (idMedida, fkSensor),
+FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
+);
+
+CREATE DATABASE gasbusters;
+USE gasbusters;
+
+
+CREATE TABLE empresa(
+idEmpresa  		INT PRIMARY KEY AUTO_INCREMENT,
+razaoSocial  	VARCHAR(100),
+nomeFantasia 	VARCHAR(100),
+responsavelLegal varchar(45),
+CNPJ  			CHAR(18),
+cidade  		VARCHAR(45),
+bairro 			VARCHAR(45),
+uf 				CHAR(5),
+rua 			VARCHAR(45),
+numero 			VARCHAR(6),
+cep 			CHAR(9),
+telefoneEmpresa CHAR(14),
+emailEmpresa 	VARCHAR(100),
+dtCriacao 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE usuario(
+idUsuario		INT AUTO_INCREMENT,
+nome  			VARCHAR(45),
+dtNasc  		DATE,
+email			VARCHAR(100),
+senha  			VARCHAR(45),
+dtCriacao  		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkEmpresa  		INT,
+PRIMARY KEY (idUsuario),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
+);
+
+
+CREATE TABLE cozinha(
+idCozinha		 	 INT AUTO_INCREMENT,
+tipoCozinha 		 VARCHAR(15),
+numeroFuncionarios 	 INT,
+observacoes 		 TEXT,
+qtdSaidaGas	 		 INT,
+fkEmpresa 			 INT,
+PRIMARY KEY (idCozinha,fkEmpresa),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+CHECK (tipoCozinha IN ('Industrial', 'Comercial'))
+);
+
+INSERT INTO cozinha VALUES (default, 'Comercial', 5, '', 3, 1);
+
+CREATE TABLE localSensor(
+idLocal INT AUTO_INCREMENT,
+descricao TEXT,
+fkCozinha INT,
+fkEmpresa INT,
+PRIMARY KEY (idLocal, fkCozinha, fkEmpresa),
+FOREIGN KEY (fkCozinha) REFERENCES cozinha(idCozinha),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE sensor(
+idSensor            INT PRIMARY KEY AUTO_INCREMENT,
+nomeSensor          VARCHAR(45),
+localInstalacao     VARCHAR(255),
+sensorStatus        VARCHAR(20),
+dtInstalacao        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkCozinha           INT,
+fkEmpresa 			INT,
+fkLocal				INT,
+FOREIGN KEY (fkCozinha) REFERENCES cozinha (idCozinha),
+FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+FOREIGN KEY (fkLocal) REFERENCES localSensor (idLocal),
+CHECK (sensorStatus IN ('Ativo', 'Inativo'))
+);
+
+CREATE TABLE  medida(
+idMedida  			INT AUTO_INCREMENT,
+nivel_gas		 	FLOAT,
+dtLeitura  			TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+fkSensor  			INT,
+PRIMARY KEY (idMedida, fkSensor),
+FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor)
+);
+
---
+++ API's/testesDataViz/src/routes/usuarios.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
+router.post("/autenticarEmail", function (req, res) {
+    usuarioController.autenticarEmail(req, res);
+});
+
+router.post("/atualizarSenha", function (req, res) {
+    usuarioController.atualizarSenha(req, res);
+});
+
+module.exports = router;
